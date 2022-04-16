2584a383f7ccf5a799b447dfc6a298f605658f1a
sterliakov <terlya.stas@gmail.com>
2022-04-17 02:52:00 +0300

    Fix script errors

--- scripts/compare_to_master.sh
+++ scripts/compare_to_master.sh
@@ -1,39 +1,49 @@
 #! /usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 cleanup() {
     git remote remove tmp_upstream > /dev/null
-    git checkout $cur_branch > /dev/null
-    exit 1
+    if [ ! -z $cur_branch ]; then
+        git checkout $cur_branch > /dev/null
+    fi
 }
 
 cur_branch=$(git branch --show-current)
-old_advice=$(git config --get advice.detachedHead) && true
 
 # Will compare to master.
-git remote add tmp_upstream https://github.com/typeddjango/django-stubs || cleanup
+git remote add tmp_upstream https://github.com/typeddjango/django-stubs || (cleanup && exit 2)
 git fetch tmp_upstream --quiet
 
 # Fetch last cache.
 mkdir -p .custom_cache/
 cur_hash=$(git rev-parse HEAD)  # Actual commit we're testing
 git fetch tmp_upstream refs/notes/*:refs/notes/*  --quiet # Use * so that it won't fail on first run
-git checkout tmp_upstream/master --quiet || cleanup
+ref_branch=tmp_upstream/master
 
-ref_hash=$(git rev-parse HEAD)  # Try to compare with master
-if [ "$ref_hash" = "$cur_hash" ]; then  # Already on master; use previous commit
-    ref_hash=$(git rev-parse HEAD^1)
+# Try to compare with master
+ref_hash=$(git rev-parse $ref_branch)
+if [ "$ref_hash" = "$cur_hash" ]; then
+    # Already on master; compare to previous commit
+    ref_hash=$(git rev-parse $ref_branch^)
 fi
-git checkout $cur_branch
 
-# Expected failure on first run
+# Get result of run on ref_hash (should fail on first workflow run)
 git notes --ref cache_history show $ref_hash > .custom_cache/.apply_errors && true
 if [ $? -eq 0 ]; then
-    ./scripts/compare_errors.py || cleanup
-    ./scripts/write_errors_cache.py
-    git notes --ref cache_history add -F .custom_cache/.apply_errors
-    git push origin refs/notes/cache_history
+    ./scripts/compare_errors.py && true
+    result=$?
 else
     echo 'No data for main branch yet - nothing to do.'
+    result=0
 fi
-cleanup || exit 0
+
+./scripts/write_errors_cache.py
+# Set identity for note
+git config --global user.email "django_stubs_test@example.com"
+git config --global user.name "Cache writing bot"
+# Always add even if it was set manually before
+git -c 'user.email=django_stubs_test@example.com' -c 'user.name=Cache writing bot' notes --ref cache_history add -f -F .custom_cache/.apply_errors $cur_hash
+git push origin refs/notes/cache_history
+
+cleanup
+exit $result
